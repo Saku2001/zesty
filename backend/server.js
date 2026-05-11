9b63a724d01e2c64a4295f9f49d8ce25b5166ce8
Saku2001 <sajetku@gmail.com>
2026-05-11 12:59:00 +0100

    fixes import booking validation

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import nodemailer from "nodemailer";
-
+import Booking from "./models/Booking.js";
 
 dotenv.config();
 
